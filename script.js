c187c19a22810b5b925ea74ef52d487a0aa64672
Olga <o.smolyakova30@gmail.com>
2022-12-10 13:30:32 -0600

    first commit, created main picture, about me, things I do section

---
+++ script.js
@@ -0,0 +1,7 @@
+gsap.from(".johnDoeText", {x:-80, duration:3, opacity:0,})
+gsap.from(".workText", {y:-80, duration:3, opacity:0,})
+gsap.from(".mainJohn", {x:200, duration:3})
+const arrayWork = ["Web Designer", "Photographer", "3D Artist"];
+
+gsap.to(".alittleMeTextH4", {rotation: 360,  duration: 1, repeat: -1, repeatDelay:6})
+gsap.from(".amazingImg", {opacity:0.1, duration:2, stagger:1, repeat:-1})
